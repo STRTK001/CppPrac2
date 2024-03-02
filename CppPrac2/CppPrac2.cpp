5cf9dc2546103c9da7ad2b586f37fa4a5f85de03
Travis Strawbridge <strtk001@mymail.unisa.edu.au>
2024-03-02 18:00:49 +1030

    Finished activity 2

--- CppPrac2/CppPrac2.cpp
+++ CppPrac2/CppPrac2.cpp
@@ -6,11 +6,12 @@
 #include "ApricotPie.h"
 #include "ApplePie.h"
 #include "RaspberryPie.h"
-
+#include <stdio.h>
 
 
 int main()
 {
+	std::cout << "ACTIVITY 1\n";
 	ApricotPie ap = ApricotPie();
 	std::cout << ap.description() <<std::endl;
 
@@ -20,5 +21,38 @@ int main()
 	RaspberryPie rp = RaspberryPie();
 	std::cout << rp.description() << std::endl;
 
+	std::cout << "ACTIVITY 2\n";
+
+	Pie* pie = nullptr;
+
+	int selection = 0;
+	do
+	{
+		printf("\nEnter a choice (1 = Apple, 2 = Apricot 3 = Raspberry): ");
+		scanf("%d", &selection);
+		switch (selection)
+		{
+		case 1:
+			pie = new ApplePie();
+			break;
+		case 2:
+			pie = new ApricotPie();
+			break;
+		case 3:
+			pie = new RaspberryPie();
+				break;
+		default:
+			break;
+		}
+		if (pie != nullptr)
+		{
+			//have to convert the string to cstr due to use of c function
+			printf("%s",pie->description().c_str());
+			break;
+		}
+		printf("You chose %d", selection);
+	}while (selection != 1 && selection != 2 && selection != 3);
+
+	printf("\nexiting program...");
 	return 0;
 }
